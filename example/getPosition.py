25282bb0b10447d50ea53cd7bece816c5c3aa577
euzl <euzl@khu.ac.kr>
2020-06-09 02:57:56 +0900

    주석추가

--- example/getPosition.py
+++ example/getPosition.py
@@ -2,13 +2,13 @@ import CoDrone
 
 def main():
     drone = CoDrone.CoDrone()
-    drone.connect()
+    drone.connect()  # 안 날고 연결만
 
     while True:
         # print the acceleration of drone
-        position = drone.get_opt_flow_position()
-        height = drone.get_height()
-        print("x={} y={} z={}".format(position.X, position.Y, height))
+        position = drone.get_opt_flow_position()  # 상대좌표 (시작 0,0)
+        height = drone.get_height()  # 고
+        print("x={} y={} z={}".format(position.X, position.Y, height))  # 단위(mm)
 
 
 if __name__ == '__main__':
