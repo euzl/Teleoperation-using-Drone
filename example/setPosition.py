cf529d8e63ddfa207be2529a5dac97247feb810c
euzl <euzl@khu.ac.kr>
2020-06-10 22:15:50 +0900

    함수 순서조정

--- example/setPosition.py
+++ example/setPosition.py
@@ -2,6 +2,12 @@ import CoDrone
 from CoDrone import Direction
 
 
+def getPosition(drone):
+    position = drone.get_opt_flow_position()  # 상대좌표 (시작 0,0)
+    height = drone.get_height()  # 고
+    print("x={} y={} z={}".format(position.X, position.Y, height))  # 단위(mm)
+
+
 def main():
     drone = CoDrone.CoDrone()
     drone.connect()
@@ -10,7 +16,7 @@ def main():
     drone.takeoff()
     drone.go(Direction.FORWARD, 3, 10)
     # drone.turn(Direction.LEFT, 3, 30)  # 돌려서 이동하려 했었다.
-    drone.go(Direction.LEFT, 7, 10)
+    drone.go(Direction.LEFT, 2, 10)
 
     getPosition(drone)
     drone.land()
@@ -20,7 +26,4 @@ if __name__ == '__main__':
     main()
 
 
-def getPosition(drone):
-    position = drone.get_opt_flow_position()  # 상대좌표 (시작 0,0)
-    height = drone.get_height()  # 고
-    print("x={} y={} z={}".format(position.X, position.Y, height))  # 단위(mm)
+
